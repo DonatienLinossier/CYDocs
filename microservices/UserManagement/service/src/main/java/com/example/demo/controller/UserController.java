81d0c4ac3d0add16f564324eb8a4fd77d209aed1
fiorina29 <fiorina2001@hotmail.fr>
2025-11-16 15:29:12 +0100

    test front backend

--- microservices/UserManagement/service/src/main/java/com/example/demo/controller/UserController.java
+++ microservices/UserManagement/service/src/main/java/com/example/demo/controller/UserController.java
@@ -18,6 +18,9 @@ import com.example.demo.security.LoginRequired;
 import com.example.demo.service.PasswordResetService;
 import com.example.demo.service.UserService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
