f49d0e8617ddcb979b837c8d67f35aa6ffe465ec
DonatienLinossier <donatien.linossier@gmail.com>
2025-11-16 14:33:34 +0100

    Test Cors

--- microservices/UserManagement/service/src/main/java/com/example/demo/security/SecurityConfig.java
+++ microservices/UserManagement/service/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,5 +14,15 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors() // enable CORS support
+            .and()
+            .csrf().disable() // disable CSRF for APIs
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // allow all requests
+
+        return http.build();
+    }
+}
