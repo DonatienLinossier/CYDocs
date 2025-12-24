751f938add84e16e2253c286871c2f124efe73c0
SABRI4 <younesysabri53@gmail.com>
2025-12-24 14:41:48 +0100

    change made to userservice

--- microservices/UserManagement/service/src/main/java/com/example/demo/security/SecurityConfig.java
+++ microservices/UserManagement/service/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,15 +2,30 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() 
+            .cors().and()    
+            .authorizeHttpRequests()
+            .anyRequest().permitAll(); 
+                                    
+        
+        return http.build();
+    }
+}
