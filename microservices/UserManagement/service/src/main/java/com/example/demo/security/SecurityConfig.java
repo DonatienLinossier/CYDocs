b4763fc3c9c2e13f0e8d991d5d8bfd104e494a92
SABRI4 <younesysabri53@gmail.com>
2025-12-24 16:37:50 +0100

    change

--- microservices/UserManagement/service/src/main/java/com/example/demo/security/SecurityConfig.java
+++ microservices/UserManagement/service/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +21,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() 
-            .cors().and()    
-            .authorizeHttpRequests()
-            .anyRequest().permitAll(); 
-                                    
+            .csrf(csrf -> csrf.disable()) 
+            .cors(cors -> cors.configure(http)) 
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            ); 
         
         return http.build();
     }
